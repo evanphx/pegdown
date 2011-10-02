d88298975cafa06b1bd2669f5a7f12f34919677f
Eric Hodel <drbrain@segment7.net>
2011-10-02 04:37:00 -0500

    Parse paragraphs

--- test/test_pegdown.rb
+++ test/test_pegdown.rb
@@ -17,15 +17,54 @@ class TestPegdown < MiniTest::Unit::TestCase
     s.force_encoding Encoding.default_external if defined? Encoding
     s.chomp
   end
-  
-  def test_parse
-    doc = parse 'it worked'
+
+  def test_parse_para
+    doc = parse "it worked\n"
 
     expected = @RM::Document.new(
-      @RM::Paragraph.new('it worked'))
+      @RM::Paragraph.new("it worked"))
 
     assert_equal expected, doc
   end
+
+  def test_parse_para_multiline
+    doc = parse "one\ntwo"
+
+    expected = @RM::Document.new(
+      @RM::Paragraph.new("one\n", "two"))
+
+    assert_equal expected, doc
+  end
+
+  def test_parse_para_two
+    doc = parse "one\n\ntwo"
+
+    expected = @RM::Document.new(
+      @RM::Paragraph.new("one"),
+      @RM::Paragraph.new("two"))
+
+    assert_equal expected, doc
+  end
+
+  def test_parse_plain
+    doc = parse "it worked"
+
+    expected = @RM::Document.new(
+      @RM::Paragraph.new("it worked"))
+
+    assert_equal expected, doc
+  end
+
+#  def test_parse_style
+#    doc = parse <<-MD
+#<style></style>
+#    MD
+#
+#    expected = @RM::Document.new(
+#      @RM::Paragraph.new("it worked"))
+#
+#    assert_equal expected, doc
+#  end
 
   def parse text
     Pegdown.parse text
